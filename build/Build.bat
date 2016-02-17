3a4704dbe689502edf03359d0d8c16c9be778fec
Collin Sauve <collinsauve@gmail.com>
2016-02-17 13:51:37 -0500

    update Build.bat

--- build/Build.bat
+++ build/Build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set params="%1"
-if %params% == "" set params=/t:unit-test
+if %params% == "" set params=/t:package /p:BUILD_NUMBER=0 /p:PackageVersion=3.0.2.2
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe build.proj /v:m %params%
 
 REM package
